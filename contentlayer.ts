7d5090c440f6a34e5763f7db6ccece41caf2bffe
Johannes Schickling <schickling.j@gmail.com>
2021-05-10 18:49:28 +0200

    schema improvements

--- contentlayer.ts
+++ contentlayer.ts
@@ -5,37 +5,21 @@ import * as path from 'path'
 import remark from 'remark'
 import html from 'remark-html'
 
-const mdToHtml = async (mdString: string) => {
-  const matterResult = matter(mdString)
-
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark().use(html).process(matterResult.content)
-
-  return processedContent.toString()
-}
-
 export const post = defineDocument(() => ({
   name: 'post',
   label: 'Post',
   filePathPattern: `**/*.md`,
+  fileType: 'md',
   fields: {
     title: {
       type: 'string',
-      label: 'Title',
       description: 'The title of the post',
       required: true,
     },
     date: {
-      type: 'string',
-      label: 'Date',
+      type: 'date',
       description: 'The date of the post',
       required: true,
-    },
-    content: {
-      type: 'markdown',
-      label: 'Page content',
-      description: 'Page content',
-      required: false,
     },
   },
   computedFields: (defineField) => [
@@ -50,3 +34,12 @@ export default defineConfig({
     contentDirPath: path.join(process.cwd(), 'posts'),
   }),
 })
+
+const mdToHtml = async (mdString: string) => {
+  const matterResult = matter(mdString)
+
+  // Use remark to convert markdown into HTML string
+  const processedContent = await remark().use(html).process(matterResult.content)
+
+  return processedContent.toString()
+}
