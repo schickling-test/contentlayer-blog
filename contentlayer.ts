ada73a49d4e58d19825fe43053b963de31885936
Johannes Schickling <schickling.j@gmail.com>
2021-05-11 13:36:11 +0200

    remove md

--- contentlayer.ts
+++ contentlayer.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from 'contentlayer/core'
 import { defineDocument, makeSourcePlugin } from 'contentlayer/source-local'
-import matter from 'gray-matter'
 import * as path from 'path'
-import remark from 'remark'
-import html from 'remark-html'
 
 export const post = defineDocument(() => ({
   name: 'post',
@@ -23,7 +20,6 @@ export const post = defineDocument(() => ({
     },
   },
   computedFields: (defineField) => [
-    defineField({ name: 'contentHTML', type: 'string', resolve: (_) => mdToHtml(_.content) }),
     defineField({ name: 'slug', type: 'string', resolve: (_) => _._id.replace('.md', '') }),
   ],
 }))
@@ -34,12 +30,3 @@ export default defineConfig({
     contentDirPath: path.join(process.cwd(), 'posts'),
   }),
 })
-
-const mdToHtml = async (mdString: string) => {
-  const matterResult = matter(mdString)
-
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark().use(html).process(matterResult.content)
-
-  return processedContent.toString()
-}
